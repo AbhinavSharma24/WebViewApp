7b581d4a0c9d6a9720e87d0c7033ac1a313a6f9d
abhinav <abhinavsharma24999@gmail.com>
2020-01-11 17:35:29 +0530

    A few changes

---
+++ app/src/main/java/com/example/webviewapp/MySiteActivity.kt
@@ -0,0 +1,34 @@
+package com.example.webviewapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.webkit.WebViewClient
+import kotlinx.android.synthetic.main.activity_my_site.*
+
+class MySiteActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_site)
+
+        webView2.webViewClient = WebViewClient()
+        webView2.loadUrl("https://abhinavsharma24.github.io/AbhinavSharmaWebsite/")
+
+        val webSettings = webView2.settings
+        webSettings.javaScriptEnabled = true
+
+        backBtn.setOnClickListener {
+            startActivity(Intent(this,MainActivity::class.java))
+        }
+    }
+
+    override fun onBackPressed() {
+        if (webView2.canGoBack()){
+            webView2.goBack()
+        }
+        else{
+            super.onBackPressed()
+        }
+    }
+}
