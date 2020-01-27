896ede54f5a291feef8a032b7014e2dea262ed5a
abhinav <abhinavsharma24999@gmail.com>
2020-01-27 21:53:47 +0530

    Some additions to code

--- app/src/main/java/com/example/webviewapp/MainActivity.kt
+++ app/src/main/java/com/example/webviewapp/MainActivity.kt
@@ -5,6 +5,7 @@ import android.app.Activity
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import android.webkit.WebViewClient
 import android.widget.Toast
 import com.firebase.ui.auth.AuthUI
@@ -18,6 +19,7 @@ class MainActivity : AppCompatActivity() {
     private val MY_REQUEST_CODE = 1234
     lateinit var providers: List<AuthUI.IdpConfig>
 
+    @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -89,5 +91,16 @@ class MainActivity : AppCompatActivity() {
             super.onBackPressed()
         }
     }
+
+    /*override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        //Inflate the menu //this adds items to the action bar if it is present.
+        //menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        //val navController = findNavController(R.id.nav_host_fragment)
+        //return navController.navigateUp(appBarConfiguration) || super.onSupportNavigateUp()
+    }*/
 
 }
